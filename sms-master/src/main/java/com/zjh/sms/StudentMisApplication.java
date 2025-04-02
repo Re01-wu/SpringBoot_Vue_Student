d318eaef2fb7e7054ff03c507b3b7917a18c5330
PoChi <3393291581@qq.com>
2025-04-02 16:12:37 +0800

    haihaihai

---
+++ sms-master/src/main/java/com/zjh/sms/StudentMisApplication.java
@@ -0,0 +1,19 @@
+package com.zjh.sms;
+
+import com.alibaba.druid.support.logging.Log;
+import com.alibaba.druid.support.logging.LogFactory;
+import com.zjh.sms.service.Upload.impl.UploadServiceImpl;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+public class StudentMisApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudentMisApplication.class, args);
+  }
+
+}
